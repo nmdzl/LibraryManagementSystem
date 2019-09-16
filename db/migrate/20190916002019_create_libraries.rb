5717197c93fc929625dff3f3ad46580cab76a43f
swen4 <swen4@ncsu.edu>
2019-09-15 20:27:38 -0400

    git commit -m "Add Library" Add Library :wq

---
+++ db/migrate/20190916002019_create_libraries.rb
@@ -0,0 +1,13 @@
+class CreateLibraries < ActiveRecord::Migration[5.1]
+  def change
+    create_table :libraries do |t|
+      t.string :name
+      t.string :university
+      t.string :location
+      t.intger :max_day
+      t.integer :overdue_fine
+
+      t.timestamps
+    end
+  end
+end

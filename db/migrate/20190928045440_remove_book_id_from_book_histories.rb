f325068b684cd44032f4755504f41a315dd912fc
swen4 <swen4@ncsu.edu>
2019-10-07 19:30:39 -0400

    add function dropdown

--- db/migrate/20190928045440_remove_book_id_from_book_histories.rb
+++ db/migrate/20190928045440_remove_book_id_from_book_histories.rb
@@ -1,6 +1,5 @@
 class RemoveBookIdFromBookHistories < ActiveRecord::Migration[5.1]
   def change
     remove_column :book_histories, :book_id
-    remove_column :book_histories, :student_id
   end
 end
